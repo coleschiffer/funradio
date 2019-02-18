8384ef91443b937287002bf3671aa32978151454
Cole Schiffer <coleschiffer@gmail.com>
2019-02-17 21:38:37 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+var audioMp3 = new Audio();
+audioMp3.src = 'http://garnet.krlx.org:8000/krlx';
+var colors =["red", "blue", "green", "yellow", "pink", "purple", "black", "orange"]
+document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
+function playAudio(){
+	audioMp3.play();
+	var play = document.getElementsByClassName("all");
+	for (var i = play.length - 1; i >= 0; i--) {
+		play[i].innerHTML = "playing the radio";
+	}
+document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
+	document.getElementById("stopper").style.display = "block";
+
+}
+
+function pauseAudio(){
+	audioMp3.pause();
+	document.getElementById("stopper").style.display = "none";
+		var play = document.getElementsByClassName("all");
+	for (var i = play.length - 1; i >= 0; i--) {
+		play[i].innerHTML = "play the radio";
+	}
+document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
+
+}
+
