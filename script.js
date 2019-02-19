b2ff07875ec50e7ed02605a67d34d28d60d43f5f
Cole Schiffer <coleschiffer@gmail.com>
2019-02-18 19:18:24 -0600

    yo

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ var audioMp3 = new Audio();
 audioMp3.src = 'http://garnet.krlx.org:8000/krlx';
 var colors = ["red", "blue", "green", "maroon", "olive", "fuchsia", "lime", "teal", "aqua", "navy", "DeepPink", "purple", "black", "orange"];
 document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
+
 function playAudio(){
 	audioMp3.play();
 	var play = document.getElementsByClassName("all");
 	for (var i = play.length - 1; i >= 0; i--) {
 		play[i].innerHTML = "playing the radio";
 	}
-document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
+	changecolor();
 	document.getElementById("stopper").style.display = "block";
 
 }
@@ -22,5 +23,8 @@ function pauseAudio(){
 	for (var i = play.length - 1; i >= 0; i--) {
 		play[i].innerHTML = "play the radio";
 	}
-document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
+}
+
+function changecolor() {
+			document.documentElement.style.setProperty('--mainColor', colors[Math.floor(Math.random()*colors.length)]);
 }
